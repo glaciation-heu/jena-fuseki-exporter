5d678b9a8a55c925c1e8e183de4b35ec06c85991
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2024-12-11 01:04:24 +0100

    Get data from Jena

--- src/jena_helm_sidecar/main.py
+++ src/jena_helm_sidecar/main.py
@@ -1,9 +1,41 @@
-from fastapi import FastAPI
+from urllib.parse import urlencode, quote
+
+import httpx
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+
+
+URL = "http://jena-fuseki.integration/slice/"
+QUERY = """SELECT (COUNT(DISTINCT ?g) AS ?numGraphs)
+WHERE {
+  GRAPH ?g {
+    # Optional condition: ?s ?p ?o .
+  }
+}"""
 
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/metrics")
 async def root():
-    return {"message": "Hello World"}
+    try:
+       graphs_number = await retrieve_graphs_number()
+    except:
+        raise HTTPException(status_code=502, detail="Bad Gateway: Upstream server error")
+    metrics = 'jena-dataset-graphs-number{dataset="slice"} ' + str(graphs_number)
+    return PlainTextResponse(metrics)
+
+
+async def retrieve_graphs_number() -> int:
+    async with httpx.AsyncClient() as client:
+        r = await client.post(
+            URL,
+            data=urlencode(
+                {"query": QUERY},
+                quote_via=quote,
+            ),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+    graphs_number = int(r.json()["results"]["bindings"][0]["numGraphs"]["value"])
+    return graphs_number
